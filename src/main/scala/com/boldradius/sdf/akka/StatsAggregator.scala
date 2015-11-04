af4be0061202e3ef6734548e36ccaf5c258195f6
Kenneth Kwan <kkwan@pagerduty.com>
2015-11-04 14:13:57 -0500

    Add types for each of the metrics that we need.

--- src/main/scala/com/boldradius/sdf/akka/StatsAggregator.scala
+++ src/main/scala/com/boldradius/sdf/akka/StatsAggregator.scala
@@ -14,4 +14,13 @@ object StatsAggregator {
   def props: Props = Props(new StatsAggregator())
 
   case class SessionData(requests: Seq[Request])
+
+  type RequestsPerBrowser = Map[String, Int]  // [browser, num_requests]
+  type RequestsByMinute = Map[Int, Int]  // [minute_starting_from_midnight, num_requests]
+  type RequestsPerPage = Map[String, Int]  // [page_name, num_requests]
+  type AverageTimePerUrl = Map[String, Int]  // [url, time_in_seconds]
+  type LandingsPerPage = Map[String, Int]  // [page_name, num_landing_requests]
+  type SinksPerPage = Map[String, Int]  // [page_num, num_sink_requests]
+  type UsersPerBrowser = Map[String, Int]  // [browser, num_users]
+  type UsersPerReferrer = Map[String, Int]  // [referrer, num_users]
 }
