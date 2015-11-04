c83b4f5a78750951db7aa35a907dd72cc1e271bf
Peter Sobot <psobot@pagerduty.com>
2015-11-04 11:56:00 -0500

    Added session timeout to SessionLog.

--- src/main/scala/com/boldradius/sdf/akka/SessionLog.scala
+++ src/main/scala/com/boldradius/sdf/akka/SessionLog.scala
@@ -1,14 +1,17 @@
 package com.boldradius.sdf.akka
 
 import scala.collection.mutable.MutableList
+import scala.concurrent.duration._
 import akka.actor._
 
-class SessionLog(sessionId: Long) extends Actor with ActorLogging {
+class SessionLog(sessionId: Long, statsActor: ActorRef) extends Actor with ActorLogging {
   log.info(s"SessionLog ${self} created for sessionId ${sessionId}")
 
   val requests = MutableList[Request]()
 
   import SessionLog._
+
+  context.setReceiveTimeout(20 seconds)
 
   override def receive: Receive = {
     case AppendRequest(request) => {
@@ -16,11 +19,18 @@ class SessionLog(sessionId: Long) extends Actor with ActorLogging {
       requests += request
     }
 
+    case ReceiveTimeout => {
+      statsActor ! "TODO REPLACE ME WITH RESULTS"
+      context stop self
+    }
+
     case msg => log.info(s"$self received message $msg")
   }
 }
 
 object SessionLog {
-  def props(sessionId: Long): Props = Props(new SessionLog(sessionId))
+  def props(sessionId: Long, statsActor: ActorRef): Props =
+    Props(new SessionLog(sessionId, statsActor))
+
   case class AppendRequest(request: Request)
 }
