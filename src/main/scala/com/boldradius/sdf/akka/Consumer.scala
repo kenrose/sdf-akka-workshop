9c71647eebd080927ad0ab5edb2366f267744613
Peter Sobot <psobot@pagerduty.com>
2015-11-04 11:13:06 -0500

    Modified Consumer to forward on requests to SessionLog.

--- src/main/scala/com/boldradius/sdf/akka/Consumer.scala
+++ src/main/scala/com/boldradius/sdf/akka/Consumer.scala
@@ -1,10 +1,15 @@
 package com.boldradius.sdf.akka
 
-import akka.actor.{Props, ActorLogging, Actor}
+import akka.actor.{ActorRef, Props, ActorLogging, Actor}
 
 class Consumer extends Actor with ActorLogging {
   override def receive: Receive = {
+    case req: Request => findOrCreateActor(req.sessionId) ! req
     case msg => log.info(s"Consumer $self received message $msg")
+  }
+
+  def findOrCreateActor(sessionId: Long): ActorRef = context.child(sessionId.toString).orElse {
+    context.actorOf(SessionLog.props, sessionId.toString)
   }
 }
 
