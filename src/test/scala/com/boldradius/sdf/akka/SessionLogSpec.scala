f5d4d2e0e117ac6d6d3e653585cb216d67ab75b3
Peter Sobot <psobot@pagerduty.com>
2015-11-04 12:25:02 -0500

    Removed unnecessary test.

--- src/test/scala/com/boldradius/sdf/akka/SessionLogSpec.scala
+++ src/test/scala/com/boldradius/sdf/akka/SessionLogSpec.scala
@@ -3,12 +3,4 @@ package com.boldradius.sdf.akka
 import akka.testkit._
 
 class SessionLogSpec extends BaseAkkaSpec {
-  "Sending any message to Consumer" should {
-    "result in logging the message" in {
-      val consumer = system.actorOf(Consumer.props, "consumer-log")
-      EventFilter.info(pattern = ".*", occurrences = 1) intercept {
-        consumer ! "Hello"
-      }
-    }
-  }
 }
