1f9a0aa252807a71768e8922f14895e100a90890
Peter Sobot <psobot@pagerduty.com>
2015-11-05 10:42:16 -0500

    EmailActorSpec tests.

--- src/test/scala/com/boldradius/sdf/akka/EmailActorSpec.scala
+++ src/test/scala/com/boldradius/sdf/akka/EmailActorSpec.scala
@@ -8,8 +8,8 @@ class EmailActorSpec extends BaseAkkaSpec {
   "Sending EmailActor a SendEmail message" should {
     "write a line to email.log" in {
       val emailActor = PdAkkaActor.createActor(system, EmailActor.Args, Some("email-actor"))
-      val message = "Failure!"
-      EventFilter.error(source = emailActor.path.toString, pattern = s".*$message.*", occurrences = 1) intercept {
+      val message = EmailActor.SendEmail("foo@bar.com", "Failure!")
+      EventFilter.error(source = emailActor.path.toString, pattern = s".*foo.*", occurrences = 1) intercept {
         emailActor ! message
       }
       system.stop(emailActor)
