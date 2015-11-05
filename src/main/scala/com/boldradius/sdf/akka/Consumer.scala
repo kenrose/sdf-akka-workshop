a508d833fbca4f87108f92a1d443fc5eeeb548e1
Kenneth Kwan <kkwan@pagerduty.com>
2015-11-05 16:09:12 -0500

    Add ConsumerApp, make Consumer use the cluster.

--- src/main/scala/com/boldradius/sdf/akka/Consumer.scala
+++ src/main/scala/com/boldradius/sdf/akka/Consumer.scala
@@ -3,8 +3,31 @@ package com.boldradius.sdf.akka
 import akka.actor._
 
 import Consumer._
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
+
 class Consumer(args: Args) extends PdAkkaActor {
+  val cluster = Cluster(context.system)
+
+  // subscribe to cluster changes, re-subscribe when restart
+  override def preStart(): Unit = {
+    //#subscribe
+    cluster.subscribe(self, initialStateMode = InitialStateAsEvents,
+      classOf[MemberEvent], classOf[UnreachableMember])
+    //#subscribe
+  }
+  override def postStop(): Unit = cluster.unsubscribe(self)
+
   override def receive: Receive = {
+    case MemberUp(member) =>
+      log.info("Member is Up: {}", member.address)
+    case UnreachableMember(member) =>
+      log.info("Member detected as unreachable: {}", member)
+    case MemberRemoved(member, previousStatus) =>
+      log.info("Member is Removed: {} after {}",
+        member.address, previousStatus)
+    case _: MemberEvent => // ignore
+
     case req: Request => {
       findOrCreateSessionLog(req.sessionId) ! SessionLog.AppendRequest(req)
     }
---
+++ src/main/scala/com/boldradius/sdf/akka/ConsumerApp.scala
@@ -0,0 +1,42 @@
+package com.boldradius.sdf.akka
+
+import com.typesafe.config.{ConfigFactory}
+import akka.actor._
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+object ConsumerApp {
+  def main(args: Array[String]): Unit = {
+    new ConsumerApp(ActorSystem("ClusterSystem", ConfigFactory.load("consumer"))).run
+  }
+}
+
+class ConsumerApp(system: ActorSystem) {
+  val settings = Settings(system)
+  val emailSender = PdAkkaActor.createActor(system, EmailActor.Args, Some("emailer"))
+  // creates a supervised actor
+  def createSupervisedActor(
+    subordinateArgs: PdAkkaActor.Args,
+    subordinateName: String): ActorRef = {
+    val maxRestarts = settings.SUPERVISOR_RESTART_COUNT
+    val supervisor = PdAkkaActor.createActor(system,
+      Supervisor.Args(subordinateArgs, subordinateName, emailSender, maxRestarts),
+      Some(s"supervisor-$subordinateName"))
+
+    implicit val executionContext = system.dispatcher
+    implicit val timeout: Timeout = settings.SUPERVISOR_STARTUP_TIMEOUT
+    val res = (supervisor ? Supervisor.GetSubordinate).mapTo[Supervisor.Subordinate]
+    Await.result(res, Duration.Inf).subordinate
+  }
+
+  val producer = system.actorOf(RequestProducer.props(100), "producerActor")
+
+  val statsAggregator = createSupervisedActor(StatsAggregator.Args, "statsAggregator")
+  val consumer = PdAkkaActor.createActor(system, Consumer.Args(statsAggregator), Some("consumer"))
+
+  def run: Unit = {
+
+  }
+}
