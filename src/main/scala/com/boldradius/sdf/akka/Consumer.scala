37446a842e9af5e211c05263c0ade1d5c656067d
Kenneth Kwan <kkwan@pagerduty.com>
2015-11-05 16:21:37 -0500

    Minor change to Consumer

--- src/main/scala/com/boldradius/sdf/akka/Consumer.scala
+++ src/main/scala/com/boldradius/sdf/akka/Consumer.scala
@@ -3,7 +3,7 @@ package com.boldradius.sdf.akka
 import akka.actor._
 
 import Consumer._
-import akka.cluster.Cluster
+import akka.cluster.{MemberStatus, Member, Cluster}
 import akka.cluster.ClusterEvent._
 
 class Consumer(args: Args) extends PdAkkaActor {
@@ -19,20 +19,25 @@ class Consumer(args: Args) extends PdAkkaActor {
   override def postStop(): Unit = cluster.unsubscribe(self)
 
   override def receive: Receive = {
+    case state: CurrentClusterState =>
+      state.members.filter(_.status == MemberStatus.Up) foreach register
     case MemberUp(member) =>
       log.info("Member is Up: {}", member.address)
-      if (member.hasRole("producer")) {
-        context.actorSelection(RootActorPath(member.address) / "user" / "producer") ! RequestProducer.ConsumerRegistration(self)
-      }
+      register(member)
     case UnreachableMember(member) =>
       log.info("Member detected as unreachable: {}", member)
     case MemberRemoved(member, previousStatus) =>
-      log.info("Member is Removed: {} after {}",
-        member.address, previousStatus)
+      log.info("Member is Removed: {} after {}", member.address, previousStatus)
     case _: MemberEvent => // ignore
 
     case req: Request => {
       findOrCreateSessionLog(req.sessionId) ! SessionLog.AppendRequest(req)
+    }
+  }
+
+  def register(member: Member): Unit = {
+    if (member.hasRole("producer")) {
+      context.actorSelection(RootActorPath(member.address) / "user" / "producer") ! RequestProducer.ConsumerRegistration(self)
     }
   }
 
