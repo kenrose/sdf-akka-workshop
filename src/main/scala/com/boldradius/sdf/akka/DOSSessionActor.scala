7329276b8f7ef7b685462069303d575287c6c82b
Peter Sobot <psobot@pagerduty.com>
2015-11-06 10:16:28 -0500

    Added DOSSessionActor

---
+++ src/main/scala/com/boldradius/sdf/akka/DOSSessionActor.scala
@@ -0,0 +1,39 @@
+package com.boldradius.sdf.akka
+
+import akka.actor._
+import System.{currentTimeMillis => now}
+import SessionActor._
+
+// Wraps around a session and emits requests to the target actor
+class DOSSessionActor(target: ActorRef) extends Actor with ActorLogging {
+
+  import context.dispatcher
+
+  // Upon actor creation, build a new session
+  val session = new Session
+
+  // This actor should only live for a certain duration, then shut itself down
+  context.system.scheduler.scheduleOnce(session.duration, self, PoisonPill)
+
+  // Start the simulation
+  self ! Click
+
+  override def receive = {
+    case Click =>
+      // Send a request to the target actor
+      val request = session.request
+      target ! request
+
+      // Schedule a Click message to myself after some time visiting this page
+      val pageDuration = Session.randomPageTime(request.url)
+      context.system.scheduler.scheduleOnce(pageDuration, self, Click)
+  }
+}
+
+object DOSSessionActor {
+
+  def props(target: ActorRef) = Props(new DOSSessionActor(target))
+
+  // Message protocol for the SessionActor
+  case object Click
+}
