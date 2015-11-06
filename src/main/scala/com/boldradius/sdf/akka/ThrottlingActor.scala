979b15cd06bda30b6aa5cae4b0894fa866ab064d
Peter Sobot <psobot@pagerduty.com>
2015-11-06 12:00:10 -0500

    Throttling!

---
+++ src/main/scala/com/boldradius/sdf/akka/ThrottlingActor.scala
@@ -0,0 +1,41 @@
+package com.boldradius.sdf.akka
+
+import akka.actor._
+import scala.concurrent.duration.Duration
+
+class ThrottlingActor(args: ThrottlingActor.Args) extends PdAkkaActor with SettingsExtension {
+  import ThrottlingActor._
+
+  private val subordinate = createChild(args.subordinateArgs, None)
+  private var previousRequests = Seq.empty[Long]
+
+  override def receive: Receive = {
+    case GetSubordinate => sender() ! Subordinate(subordinate)
+    case msg: SessionLog.AppendRequest => throttle(msg)
+  }
+
+  def throttle(msg: Any): Unit = {
+    val incomingTime = System.currentTimeMillis()
+    if (previousRequests.size < args.numberOfRequests) {
+      subordinate.forward(msg)
+      previousRequests = previousRequests :+ incomingTime
+    } else if ((incomingTime - previousRequests.head) > args.period.toMillis) {
+      subordinate.forward(msg)
+      previousRequests = previousRequests.drop(1) :+ incomingTime
+    } else {
+      // drop this request on the floor
+      log.warning(s"Dropped a request on the floor: $msg. Next opening for a request at ${previousRequests.head + args.period.toMillis}")
+    }
+  }
+}
+
+object ThrottlingActor {
+  case class Args(
+                   subordinateArgs: PdAkkaActor.Args,
+                   numberOfRequests: Int,
+                   period: Duration)
+    extends PdAkkaActor.Args(classOf[ThrottlingActor])
+
+  case object GetSubordinate
+  case class Subordinate(subordinate: ActorRef)
+}
